7be9c7b4e7918d5f67687a20b338d2a9918dcfb0
Giovane <giovaneaalmeida27@gmail.com>
2021-10-28 14:33:09 -0300

    feat: createConnection.then

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,28 +1,34 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { createConnection } from 'typeorm';
 
-// const Connection = require('../database/connection');
 
-import { getRepository } from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 
-import UserController from '../controllers/UserController';
-import UserService from '../services/UserService';
+import { UserController } from '../controllers/UserController';
+import { UserService } from '../services/UserService';
 
+import { UserRepository } from '../repositories/UserRepository';
 import AuthenticateUserController from '../controllers/AuthenticateUserController';
 import AuthenticateUserService from '../services/AuthenticateUserService';
 
-const userRepository = getRepository('User')
+createConnection().then(() => {
 
-const userService = new UserService(userRepository);
-const userController = new UserController(userService);
-const authenticateUserService = new AuthenticateUserService(userRepository);
-const authenticateUserController = new AuthenticateUserController(authenticateUserService);
+  const userRepository = getCustomRepository(UserRepository);
 
-const app = express();
-app.use(express.json());
+  const userService = new UserService(userRepository);
+  const userController = new UserController(userService);
+  const authenticateUserService = new AuthenticateUserService(userRepository);
+  const authenticateUserController = new AuthenticateUserController(authenticateUserService);
 
-app.post('/login', authenticateUserController.handleAuthenticateUser)
-app.post('/users', userController.createUser);
+  const app = express();
+  app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  app.post('/login', authenticateUserController.handleAuthenticateUser)
+  app.post('/user', async (request: Request, response: Response) => {
+    return await userController.createUser(request, response);
+  });
+
+  app.listen(3000, () => {
+    console.log('Server started on port 3000');
+  });
 });
