c90880ae58b18191b900d8796c86e3b7c48cfa54
Giovane <giovaneaalmeida27@gmail.com>
2021-10-28 22:15:21 -0300

    refact: move composition logic to a separated file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,44 +1,17 @@
-import express, { Request, Response } from 'express';
+import express, { Router } from 'express';
 import { createConnection } from 'typeorm';
-
-import { getCustomRepository } from 'typeorm';
-
-import { UserController } from '../controllers/UserController';
-import { UserService } from '../services/UserService';
-
-import { UserRepository } from '../repositories/UserRepository';
-import AuthenticateUserController from '../controllers/AuthenticateUserController';
-import AuthenticateUserService from '../services/AuthenticateUserService';
-
-import { ensureAutheticate } from '../middlewares/ensureAutheticate';
+import { UserComposer } from '../composers/userComposer';
+import '../database'
 
 createConnection().then(() => {
 
-  const userRepository = getCustomRepository(UserRepository);
-
-  const userService = new UserService(userRepository);
-  const userController = new UserController(userService);
-  const authenticateUserService = new AuthenticateUserService(userRepository);
-  const authenticateUserController = new AuthenticateUserController(authenticateUserService);
-
   const app = express();
+  const userRouter = Router();
+
+  UserComposer.compose(userRouter);
+
   app.use(express.json());
-
-  app.post('/login', async (request: Request, response: Response) => {
-    return await authenticateUserController.handleAuthenticateUser(request, response);
-  })
-
-  app.post('/user', async (request: Request, response: Response) => {
-    return await userController.createUser(request, response);
-  });
-
-  app.put('/user', ensureAutheticate, async (request: Request, response: Response) => {
-    return await userController.updateUser(request, response);
-  });
-
-  app.delete('/user', ensureAutheticate, async (request: Request, response: Response) => {
-    return await userController.deleteUser(request, response);
-  });
+  app.use(userRouter);
 
   app.listen(3000, () => {
     console.log('Server started on port 3000');
